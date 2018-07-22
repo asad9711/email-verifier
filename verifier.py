db4d541712e8d221509b2a0aac7b761b57ebaa49
Asad Hussain <asad@voereir.com>
2018-07-22 22:09:47 +0530

    script and data file

---
+++ verifier.py
@@ -0,0 +1,31 @@
+from dns import resolver
+import smtplib
+import ConfigParser
+import os
+
+config = ConfigParser.ConfigParser()
+config.read(os.path.join(os.getcwd(), 'cfg_data.conf'))
+
+rcpt_mail_domain = config.get('data','domain_of_mail')
+rcpt_mail_id = config.get('data', 'mail_id_to_validate')
+sender_mail_id = config.get('data', 'sender_mail_id')
+
+# TO know more about MX record of DNS server, refer
+# https://www.slashroot.in/mx-record-dns-explained-example-configurations
+
+dns_records = resolver.query(rcpt_mail_domain, 'MX')
+mx_record = dns_records[0].exchange
+mx_record = str(mx_record)
+
+smtp_server = smtplib.SMTP()
+smtp_server.set_debuglevel(0)
+
+smtp_server.connect(mx_record)
+smtp_server.mail(sender=sender_mail_id)
+status_code, resp_msg = smtp_server.rcpt(recip=rcpt_mail_id)
+smtp_server.quit()
+
+if status_code == 250:
+    print 'mail-id %s is valid' % rcpt_mail_id
+else:
+    print 'mail-id %s is NOT valid' % rcpt_mail_id
